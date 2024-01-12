1fb99f3c79f620a4f3a6cfb93baa2463892d8b17
viniciszs <114860862+viniciszs@users.noreply.github.com>
2024-01-11 21:26:30 -0300

    add scalar types

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
-import {gql, ApolloServer} from 'apollo-server' 
+import { gql, ApolloServer } from 'apollo-server'
 
+
+
+const typeDefs = gql`
+    type Query{
+      name: String,
+      age: Int,
+      isHuman: Boolean,
+      number: Float
+    }
+  `
 
 const resolvers = {
   Query: {
-    hello(){
-      return 'hello world!'
+    name() {
+      return 'Vinnicius'
+    },
+    age() {
+      return 23
+    },
+    isHuman(){
+      return true
+    },
+    number(){
+      return 9.43254242
     }
   }
 }
 
-const typeDefs = gql`
-  type Query{
-    hello: String
-  }
-`
 
 const server = new ApolloServer({
   typeDefs,
---
+++ src/typesScalares.ts
@@ -0,0 +1,47 @@
+/* 
+  * Scalar Types
+
+  - Int
+  - Float
+  - String
+  - Boolean
+  - ID
+*/
+
+// import { gql, ApolloServer } from 'apollo-server'
+
+
+
+// const typeDefs = gql`
+//     type Query{
+//       name: String,
+//       age: Int,
+//       isHuman: Boolean,
+//       number: Float
+//     }
+//   `
+
+// const resolvers = {
+//   Query: {
+//     name() {
+//       return 'Vinnicius'
+//     },
+//     age() {
+//       return 23
+//     },
+//     isHuman(){
+//       return true
+//     },
+//     number(){
+//       return 9.43254242
+//     }
+//   }
+// }
+
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers
+// })
+
+// server.listen()
